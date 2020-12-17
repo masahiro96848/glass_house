bea90a967f96e0dce309f449cae2498a571d5751
masahiro96848 <feetwcpic9436@gmail.com>
2020-12-18 08:44:03 +0900

    Musqlの接続完了 env設定してマイグレーションができた

--- laravel/database/migrations/2014_10_12_000000_create_users_table.php
+++ laravel/database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,11 +14,15 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->string('name');
+            $table->increments('id');
+            $table->string('name', 20);
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('intro', 200)->nullable();
+            $table->string('profile_image')->nullable();
+            $table->string('talk_theme')->nullanle();
+            $table->string('speaking')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
