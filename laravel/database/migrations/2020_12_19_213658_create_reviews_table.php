e031c83b45d5cf4aab9cecc4beefd3e9482e1896
masahiro96848 <feetwcpic9436@gmail.com>
2020-12-19 22:06:14 +0900

    reviewテーブルを作成

---
+++ laravel/database/migrations/2020_12_19_213658_create_reviews_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateReviewsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reviews', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->integer('star');
+            $table->string('body');
+            $table->integer('reviewer_id')->unsigned();
+            $table->integer('reviewed_id')->unsigned();
+            $table->foreign('reviewer_id')->references('id')->on('users')->OnDelete('cascade');
+            $table->foreign('reviewed_id')->references('id')->on('users')->OnDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reviews');
+    }
+}
