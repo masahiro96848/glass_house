e187e1b885f8a0a4ec007fb042439fe702ebbbea
masahiro96848 <feetwcpic9436@gmail.com>
2020-12-19 14:26:12 +0900

    jobs 一覧画面のデータ表示機能を追加しました

--- laravel/resources/views/meeting/index.blade.php
+++ laravel/resources/views/meeting/index.blade.php
@@ -32,94 +32,30 @@
           </a>
         </div>
         <div class="p-card">
-          <div class="p-card--area">
-            <div class="p-card--content">
-              <div class="p-card--photo">
-                <img src="../img/wed.jpeg" alt=""class="c-user--image" >
-              </div>
-              <div class="p-card--body">
-                <div class="p-card--detail">
-                  <h4 class="p-card--title">ゆるっと何度でもお気軽にどうぞ</h4>
+          @foreach($jobs as $job)
+            <div class="p-card--area">
+              <div class="p-card--content">
+                <div class="p-card--photo">
+                  <img src="../img/wed.jpeg" alt=""class="c-user--image" >
                 </div>
-                <p class="p-card--paragragh">仕事の話でも、そうでなくても、なんでもゆるっと気軽に話しませんか。 本日15時くらいまでの間でお時間合う方、よろしくお願いします。</p>
-                <div class="p-card--name">
-                  <h5 class="p-card--userName">masahiro</h5>
+                <div class="p-card--box">
+                  <div class="p-card--detail">
+                    <h4 class="p-card--title">{{ $job->title}}</h4>
+                  </div>
+                  <p class="p-card--paragragh">{{ $job->summary}}</p>
+                  <div class="p-card--name">
+                    <h5 class="p-card--userName">masahiro</h5>
+                  </div>
                 </div>
-              </div>
-              <div class="p-card--right">
-                <p class="p-card--apply">申請する</p>
-                <p class="p-card--star">🌟🌟🌟🌟🌟</p>
-                <p class="pcard--starCount">5.0</p>
-                <p class="pcard--reviewCount">レビュー5件</p>
+                <div class="p-card--right">
+                  <p class="p-card--apply">申請する</p>
+                  <p class="p-card--star">🌟🌟🌟🌟🌟</p>
+                  <p class="pcard--starCount">5.0</p>
+                  <p class="pcard--reviewCount">レビュー5件</p>
+                </div>
               </div>
             </div>
-          </div>
-          <div class="p-card--area">
-            <div class="p-card--content">
-              <div class="p-card--photo">
-                <img src="../img/wed.jpeg" alt=""class="c-user--image" >
-              </div>
-              <div class="p-card--body">
-                <div class="p-card--detail">
-                  <h4 class="p-card--title">ゆるっと何度でもお気軽にどうぞ</h4>
-                </div>
-                <p class="p-card--paragragh">仕事の話でも、そうでなくても、なんでもゆるっと気軽に話しませんか。 本日15時くらいまでの間でお時間合う方、よろしくお願いします。</p>
-                <div class="p-card--name">
-                  <h5 class="p-card--userName">masahiro</h5>
-                </div>
-              </div>
-              <div class="p-card--right">
-                <p class="p-card--apply">申請する</p>
-                <p class="p-card--star">🌟🌟🌟🌟🌟</p>
-                <p class="pcard--starCount">5.0</p>
-                <p class="pcard--reviewCount">レビュー5件</p>
-              </div>
-            </div>
-          </div>
-          <div class="p-card--area">
-            <div class="p-card--content">
-              <div class="p-card--photo">
-                <img src="../img/wed.jpeg" alt=""class="c-user--image" >
-              </div>
-              <div class="p-card--body">
-                <div class="p-card--detail">
-                  <h4 class="p-card--title">ゆるっと何度でもお気軽にどうぞ</h4>
-                </div>
-                <p class="p-card--paragragh">仕事の話でも、そうでなくても、なんでもゆるっと気軽に話しませんか。 本日15時くらいまでの間でお時間合う方、よろしくお願いします。</p>
-                <div class="p-card--name">
-                  <h5 class="p-card--userName">masahiro</h5>
-                </div>
-              </div>
-              <div class="p-card--right">
-                <p class="p-card--apply">申請する</p>
-                <p class="p-card--star">🌟🌟🌟🌟🌟</p>
-                <p class="pcard--starCount">5.0</p>
-                <p class="pcard--reviewCount">レビュー5件</p>
-              </div>
-            </div>
-          </div>
-          <div class="p-card--area">
-            <div class="p-card--content">
-              <div class="p-card--photo">
-                <img src="../img/wed.jpeg" alt=""class="c-user--image" >
-              </div>
-              <div class="p-card--body">
-                <div class="p-card--detail">
-                  <h4 class="p-card--title">ゆるっと何度でもお気軽にどうぞ</h4>
-                </div>
-                <p class="p-card--paragragh">仕事の話でも、そうでなくても、なんでもゆるっと気軽に話しませんか。 本日15時くらいまでの間でお時間合う方、よろしくお願いします。</p>
-                <div class="p-card--name">
-                  <h5 class="p-card--userName">masahiro</h5>
-                </div>
-              </div>
-              <div class="p-card--right">
-                <p class="p-card--apply">申請する</p>
-                <p class="p-card--star">🌟🌟🌟🌟🌟</p>
-                <p class="pcard--starCount">5.0</p>
-                <p class="pcard--reviewCount">レビュー5件</p>
-              </div>
-            </div>
-          </div>
+          @endforeach
         </div>
       </div>
     </div>
