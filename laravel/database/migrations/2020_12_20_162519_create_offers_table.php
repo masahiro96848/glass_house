c13f3522bd0bbeb3a7eebe0678bb46832b14a254
masahiro96848 <feetwcpic9436@gmail.com>
2020-12-21 09:59:21 +0900

    オファーテーブル作成

---
+++ laravel/database/migrations/2020_12_20_162519_create_offers_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOffersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('offers', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('offer_id')->unsigned();
+            $table->integer('offered_id')->unsigned();
+            $table->enum('status', ['承認待ち', '承認済み', '取消済み']);
+            $table->foreign('offer_id')->references('id')->on('users')->OnDelete('cascade');
+            $table->foreign('offered_id')->references('id')->on('users')->OnDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('offers');
+    }
+}
