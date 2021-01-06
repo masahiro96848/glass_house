39676c7cd0b6171cefafcdc0ab1c099b33a5be31
masahiro96848 <feetwcpic9436@gmail.com>
2021-01-06 21:44:59 +0900

    レビュー機能を追加しました　データ入力が上手くいきました

--- laravel/app/Review.php
+++ laravel/app/Review.php
@@ -7,8 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 class Review extends Model
 {
     protected $fillable = [
-        'title', 'star', 'body', 'reviewer_id', 'reviewed_id',
+        'title', 'star', 'body', 'reviewer_id', 'reviewed_id', 'matching_id',
     ];
 
-    
+    public function reviewed()
+    {
+        return $this->belongsTo(User::class, 'reviewed_id');
+    }
 }
