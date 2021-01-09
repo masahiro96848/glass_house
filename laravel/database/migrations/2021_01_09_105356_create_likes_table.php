f99e74a257a260f47b900edd6fd620dc42dab107
masahiro96848 <feetwcpic9436@gmail.com>
2021-01-09 11:06:22 +0900

    likesマイグレーションファイルの作成

---
+++ laravel/database/migrations/2021_01_09_105356_create_likes_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLikesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('likes', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('liking_id')->unsigned();
+            $table->integer('liked_id')->unsigned();
+            $table->foreign('liking_id')->references('id')->on('users')->OnDelete('cascade');
+            $table->foreign('liked_id')->references('id')->on('users')->OnDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('likes');
+    }
+}
