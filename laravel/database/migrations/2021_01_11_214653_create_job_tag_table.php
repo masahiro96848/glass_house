ce39e672631f56184ad71fd474c1889968abc4bd
masahiro96848 <feetwcpic9436@gmail.com>
2021-01-11 21:49:36 +0900

    tags、 job_tagのマイグレーションファイルを作成

---
+++ laravel/database/migrations/2021_01_11_214653_create_job_tag_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateJobTagTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('job_tag', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('job_id')->unsigned();
+            $table->integer('tag_id')->unsigned();
+            $table->foreign('job_id')->references('id')->on('jobs')->OnDelete('cascade');
+            $table->foreign('tag_id')->references('id')->on('tags')->OnDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('job_tag');
+    }
+}
