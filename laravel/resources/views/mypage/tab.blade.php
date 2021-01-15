1bc8f8ef216c01f57b5eb8f09b47c4e5ad366a9a
masahiro96848 <feetwcpic9436@gmail.com>
2021-01-15 09:11:32 +0900

    マイページのlike liked matching　bladeの共通化

---
+++ laravel/resources/views/mypage/tab.blade.php
@@ -0,0 +1,11 @@
+<div class="p-card--tabArea">
+  <div class="p-card--tab">
+    <a href="{{ route('mypage.liking')}}"><h5 class="p-card--button">気になるリスト</h5></a>
+  </div>
+  <div class="p-card--tab">
+    <a href="{{ route('mypage.liked')}}"><h5 class="p-card--button">あなたと話してみたいリスト</h5></a>
+  </div>
+  <div class="p-card--tab">
+    <a href="{{ route('mypage.matching')}}"><h5 class="p-card--button">マッチングリスト</h5></a>
+  </div>
+</div>
