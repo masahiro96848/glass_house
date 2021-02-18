f5ec565c2a88a32a06d1b3b3a0c55c1559b8923b
masahiro96848 <feetwcpic9436@gmail.com>
2021-02-18 09:16:08 +0900

    LPページの修正　headerの修正

--- laravel/resources/views/mypage/tab.blade.php
+++ laravel/resources/views/mypage/tab.blade.php
@@ -1,12 +1,12 @@
 <div class="p-card--tabArea">
   <div class="p-card--tab">
+    <a href="{{ route('mypage.matching')}}"><h5 class="p-card--button">マッチングリスト</h5></a>
+  </div>
+  <div class="p-card--tab">
     <a href="{{ route('mypage.liking')}}"><h5 class="p-card--button">気になるリスト</h5></a>
   </div>
   <div class="p-card--tab">
     <a href="{{ route('mypage.liked')}}"><h5 class="p-card--button">あなたと話してみたいリスト</h5></a>
-  </div>
-  <div class="p-card--tab">
-    <a href="{{ route('mypage.matching')}}"><h5 class="p-card--button">マッチングリスト</h5></a>
   </div>
   <div class="p-card--tab">
     <a href="{{ route('mypage.job')}}"><h5 class="p-card--button">テーマリスト</h5></a>
