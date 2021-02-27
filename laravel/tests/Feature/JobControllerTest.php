18051e2a744a85b72369cf919008dea33319f5ce
masahiro96848 <feetwcpic9436@gmail.com>
2021-02-27 12:52:55 +0900

    jobの登録機能のテストコードを追加

--- laravel/tests/Feature/JobControllerTest.php
+++ laravel/tests/Feature/JobControllerTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature;
 
 use App\User;
+use App\Job;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
@@ -17,10 +18,19 @@ class JobControllerTest extends TestCase
      */
     use RefreshDatabase;
 
+
+    // 新規投稿
     // 未ログイン時のテスト
     public function testGuestNew()
     {
         $response = $this->get(route('job.new'));
+
+        $response->assertRedirect(route('login'));
+    }
+
+    public function testGuestCreate()
+    {
+        $response = $this->post(route('job.create'));
 
         $response->assertRedirect(route('login'));
     }
@@ -33,5 +43,31 @@ class JobControllerTest extends TestCase
         $response = $this->actingAs($user)->get(route('job.new'));
 
         $response->assertStatus(200)->assertViewIs('job.new');
+    }
+
+    public function testAuthCreate()
+    {
+        // テストデータを追加(UserとJobのデータを追加)
+        $user = factory(User::class)->create();
+        $title = 'テストタイトル';
+        $summary = 'テスト内容';
+        $user_id = $user->id;
+
+        // post送信して実行する
+        $response = $this->actingAs($user)->post(route('job.create', [
+            'title' => $title,
+            'summary' => $summary,
+            'user_id' => $user_id,
+        ]));
+
+        // テストデータが登録しているかを確認
+        $this->assertDatabaseHas('jobs', [
+            'title' => $title,
+            'summary' => $summary,
+            'user_id' => $user_id,
+        ]);
+
+        $response->assertRedirect(route('job.index'));
+
     }
 }
