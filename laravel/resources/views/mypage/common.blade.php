b58bd86305b1704303e5e1149c97420f23980f3d
masahiro96848 <feetwcpic9436@gmail.com>
2021-03-11 15:51:24 +0900

    データがないときにコメントが表示されるように修正

--- laravel/resources/views/mypage/common.blade.php
+++ laravel/resources/views/mypage/common.blade.php
@@ -1,38 +1,42 @@
 <div class="p-card">
-  @foreach($users as $user)
-    <div class="p-card--area">
-      <div class="p-card--content">
-        <div class="p-card--photo">
-          <a href="{{ route('users.show', ['name' => $user->name])}}">
-            <img src="../img/wed.jpeg" alt=""class="c-user--image" >
-          </a> 
-        </div>
-        <div class="p-card--box">
-          <div class="p-card--detail">
-            <h4 class="p-card--title">{{ $user->name }}</h4>
+  @if(count($users) >= 1)
+    @foreach($users as $user)
+      <div class="p-card--area">
+        <div class="p-card--content">
+          <div class="p-card--photo">
+            <a href="{{ route('users.show', ['name' => $user->name])}}">
+              <img src="../img/wed.jpeg" alt=""class="c-user--image" >
+            </a> 
           </div>
-          <p class="p-card--paragragh">{{ $user->intro}}</p>
-          <div class="p-card--name">
-            <h5 class="p-card--userName"></h5>
+          <div class="p-card--box">
+            <div class="p-card--detail">
+              <h4 class="p-card--title">{{ $user->name }}</h4>
+            </div>
+            <p class="p-card--paragragh">{{ $user->intro}}</p>
+            <div class="p-card--name">
+              <h5 class="p-card--userName"></h5>
+            </div>
+            
           </div>
-          
-        </div>
-        <div class="p-card--right">
-          
-            <a href="{{ route('offer.confirm', ['name' => $user->name])}}"><p class="p-card--apply">話してみたい</p></a>
-          
-          <p class="c-user--likes">
-            <user-like
-              :initial-liked-by='@json($user->isLikedBy(Auth::user()))'
-              :initial-count-likes='@json($user->count_likes)'
-              :authorized='@json(Auth::check())'
-              endpoint="{{ route('users.like', ['id' => $user->id])}}"
-            >
-            </user-like>
-          </p>
-            <p class="pcard--reviewCount">レビュー{{ $user->revieweds()->count()}} 件</p>
+          <div class="p-card--right">
+            
+              <a href="{{ route('offer.confirm', ['name' => $user->name])}}"><p class="p-card--apply">話してみたい</p></a>
+            
+            <p class="c-user--likes">
+              <user-like
+                :initial-liked-by='@json($user->isLikedBy(Auth::user()))'
+                :initial-count-likes='@json($user->count_likes)'
+                :authorized='@json(Auth::check())'
+                endpoint="{{ route('users.like', ['id' => $user->id])}}"
+              >
+              </user-like>
+            </p>
+              <p class="pcard--reviewCount">レビュー{{ $user->revieweds()->count()}} 件</p>
+          </div>
         </div>
       </div>
-    </div>
-  @endforeach
+    @endforeach
+  @else
+    <p class="p-card--nodata">いいねはありません。</p>
+  @endif
 </div>
--- laravel/resources/views/mypage/job.blade.php
+++ laravel/resources/views/mypage/job.blade.php
@@ -9,56 +9,60 @@
       <div class="l-container--layout--80">
         @include('mypage.tab')
         <div class="p-card">
-          @foreach($jobs as $job)
-            <div class="p-card--area">
-              <div class="p-card--content">
-                <div class="p-card--box p-card--box--mypage">
-                  <div class="p-card--detail">
-                    <a href="{{ route('job.show', [$job->id])}}">
-                      <h4 class="p-card--title">{{ $job->title}}</h4>
-                    </a>
-                    @if(Auth::id() === $job->user->id)
-                        <a href="{{ route('job.edit', ['id' => $job->id])}}">
-                          <i class="p-panel--edit fa fa-pencil-square fa-lg" aria-hidden="true"></i>
-                        </a>
-                        <a href="" class="c-modal--open">
-                          <i class="fa fa-trash fa-lg" aria-hidden="true"></i>
-                        </a>
-                        <div class="c-modal c-modal--js">
-                          <div class="c-modal--bg c-modal--close"></div>
-                          <div class="c-modal--display">
-                            <p class="c-modal--title">テーマを削除しますか？</p>
-                            <div class="c-modal--select">
-                              <form action="{{ route('job.delete', ['id' => $job->id])}}" method="POST" class="p-panel--trash">
-                                @csrf
-                                @method('DELETE')
-                                <button class="p-panel--trash--button" onfocus="this.blur();" >
-                                  <p class="c-modal--delete">削除する</p>
-                                </button>
-                              </form>
-                              <a href="" class="c-modal--close c-modal--cancel">キャンセル</a>
+          @if(count($jobs) >= 1)
+            @foreach($jobs as $job)
+              <div class="p-card--area">
+                <div class="p-card--content">
+                  <div class="p-card--box p-card--box--mypage">
+                    <div class="p-card--detail">
+                      <a href="{{ route('job.show', [$job->id])}}">
+                        <h4 class="p-card--title">{{ $job->title}}</h4>
+                      </a>
+                      @if(Auth::id() === $job->user->id)
+                          <a href="{{ route('job.edit', ['id' => $job->id])}}">
+                            <i class="p-panel--edit fa fa-pencil-square fa-lg" aria-hidden="true"></i>
+                          </a>
+                          <a href="" class="c-modal--open">
+                            <i class="fa fa-trash fa-lg" aria-hidden="true"></i>
+                          </a>
+                          <div class="c-modal c-modal--js">
+                            <div class="c-modal--bg c-modal--close"></div>
+                            <div class="c-modal--display">
+                              <p class="c-modal--title">テーマを削除しますか？</p>
+                              <div class="c-modal--select">
+                                <form action="{{ route('job.delete', ['id' => $job->id])}}" method="POST" class="p-panel--trash">
+                                  @csrf
+                                  @method('DELETE')
+                                  <button class="p-panel--trash--button" onfocus="this.blur();" >
+                                    <p class="c-modal--delete">削除する</p>
+                                  </button>
+                                </form>
+                                <a href="" class="c-modal--close c-modal--cancel">キャンセル</a>
+                              </div>
                             </div>
                           </div>
+                        @endif
+                    </div>
+                    <p class="p-card--paragragh">{{ $job->summary}}</p>
+                    @foreach($job->tags as $tag)
+                      @if($loop->first)
+                        <div class="p-card--tag">
+                      @endif
+                        <a href="{{ route('tag.index', ['name' => $tag->name])}}">
+                          {{ $tag->hashtag }}
+                        </a>
+                      @if($loop->last)
                         </div>
                       @endif
+                    @endforeach
+                    <p class="p-card--date">{{ $job->created_at->format('Y-m-d')}}</p>
                   </div>
-                  <p class="p-card--paragragh">{{ $job->summary}}</p>
-                  @foreach($job->tags as $tag)
-                    @if($loop->first)
-                      <div class="p-card--tag">
-                    @endif
-                      <a href="{{ route('tag.index', ['name' => $tag->name])}}">
-                        {{ $tag->hashtag }}
-                      </a>
-                    @if($loop->last)
-                      </div>
-                    @endif
-                  @endforeach
-                  <p class="p-card--date">{{ $job->created_at->format('Y-m-d')}}</p>
                 </div>
               </div>
-            </div>
-          @endforeach
+            @endforeach
+          @else
+            <p class="p-card--nodata">投稿しているトークテーマはありません</p>
+          @endif
         </div>
       </div>
     </div>
