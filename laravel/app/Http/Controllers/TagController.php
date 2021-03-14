22fc072db03be22c7227725278fe5513a737f014
masahiro96848 <feetwcpic9436@gmail.com>
2021-03-14 19:11:02 +0900

    タグ検索ページにタグ検索エリアを作成

--- laravel/app/Http/Controllers/TagController.php
+++ laravel/app/Http/Controllers/TagController.php
@@ -10,9 +10,11 @@ class TagController extends Controller
     public function index($name)
     {
         $tag = Tag::where('name', $name)->first();
+        $tag_jobs = Tag::withCount('jobs')->orderBy('jobs_count', 'desc')->take(10)->get();
 
         return view('tag.index', [
             'tag' => $tag,
+            'tag_jobs' => $tag_jobs,
         ]);
     }
 }
