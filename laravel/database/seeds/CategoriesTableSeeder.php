aae67d2af26b56338543523705bf278eaac03dba
masahiro96848 <feetwcpic9436@gmail.com>
2021-03-25 13:53:04 +0900

    categoryのseederの処理内容を修正

--- laravel/database/seeds/CategoriesTableSeeder.php
+++ laravel/database/seeds/CategoriesTableSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Category;
 
 class CategoriesTableSeeder extends Seeder
 {
@@ -12,32 +13,37 @@ class CategoriesTableSeeder extends Seeder
     public function run()
     {
         $categories = [
-            ['id' => 1, 'name' => 'エンジニア'],
-            ['id' => 2, 'name' => 'デザイナー'],
-            ['id' => 3, 'name' => 'マーケティング'],
-            ['id' => 4, 'name' => 'ディレクター'],
-            ['id' => 5, 'name' => 'コンサルタント'],
-            ['id' => 6, 'name' => '起業家'],
-            ['id' => 7, 'name' => 'YouTuber'],
-            ['id' => 8, 'name' => 'アーティスト'],
-            ['id' => 9, 'name' => '動画・映像クリエイター'],
-            ['id' => 10, 'name' => 'ディレクター'],
-            ['id' => 11, 'name' => '営業・セールス'],
-            ['id' => 12, 'name' => 'ブロガー'],
-            ['id' => 13, 'name' => 'Instagramer'],
-            ['id' => 14, 'name' => '人事・採用'],
-            ['id' => 15, 'name' => 'ライター'],
-            ['id' => 16, 'name' => 'カメラマン'],
-            ['id' => 17, 'name' => '税理士'],
-            ['id' => 18, 'name' => '弁護士'],
-            ['id' => 19, 'name' => '公認会計士'],
-            ['id' => 20, 'name' => '社会保険労務士'],
-            ['id' => 21, 'name' => '料理家'],
-            ['id' => 22, 'name' => '医師'],
-            ['id' => 23, 'name' => '学生'],
-            ['id' => 24, 'name' => '教師'],
-
+            'エンジニア',
+            'デザイナー',
+            'マーケティング',
+            'ディレクター',
+            'コンサルタント',
+            '起業家',
+            'YouTuber',
+            'アーティスト',
+            '動画・映像クリエイター',
+            'ディレクター',
+            '営業・セールス',
+            'ブロガー',
+            'Instagramer',
+            '人事・採用',
+            'ライター',
+            'カメラマン',
+            '税理士',
+            '弁護士',
+            '公認会計士',
+            '社会保険労務士',
+            '料理家',
+            '医師',
+            '学生',
+            '教師',
+            '漫画家',
+            'アスリート'
         ];
-        DB::table('categories')->insert($categories);
+        foreach($categories as $category) {
+            Category::updateOrCreate([
+                'name' => $category
+            ]);
+        }
     }
 }
