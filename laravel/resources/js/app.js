67bcac66789a8291745fc3cdb8f5a6ed97776457
masahiro96848 <feetwcpic9436@gmail.com>
2021-05-13 10:47:11 +0900

    LPとheaderのレスポンシブ対応追加

--- laravel/resources/js/app.js
+++ laravel/resources/js/app.js
@@ -28,15 +28,6 @@ const app  = new Vue({
   }
 });
 
-$('.p-menu--button').on('click', function() {
-  if( $(this).hasClass('active')){
-    $(this).removeClass('active');
-    $('.p-menu--wrap').addClass('close').removeClass('open');
-  }else {
-    $(this).addClass('active');
-    $('.p-menu--wrap').addClass('open').removeClass('close');
-  }
-});
 
 // フラッシュメッセージ
 $(function() {
