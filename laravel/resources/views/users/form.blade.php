2be4ccc80d3807410cb064b9ab3198b7366e3288
masahiro96848 <feetwcpic9436@gmail.com>
2021-05-15 12:52:15 +0900

    レスポンシブ対応追加

--- laravel/resources/views/users/form.blade.php
+++ laravel/resources/views/users/form.blade.php
@@ -1,5 +1,5 @@
 <div class="c-user--Container">
-  <h5 class="c-user--featureTitle">ユーザー名</h5>
+  <h5 class="c-user--featureTitle--review">ユーザー名</h5>
     <div class="c-user--featureArea">
       <div class="c-user">
         @if($current_user !== $matching->apply_id && $matching->approve_id)
@@ -8,20 +8,20 @@
           @else
             <img src="{{$matching->apply->profile_image}}" alt="" class="c-user--image--sm">  
           @endif
-            <span class="">{{ $matching->apply->name}}</span>  
+            <span class="c-user--name--review">{{ $matching->apply->name}}</span>  
         @else
           @if(!isset($matching->approve->profile_image))
             <img src="{{asset('img/no_image.jpg')}}" alt="" class="c-user--image--sm">
           @else
             <img src="{{$matching->approve->profile_image}}" alt="" class="c-user--image--sm">  
           @endif
-            <span class="">{{ $matching->approve->name}}</span>
+            <span class="c-user--name--review">{{ $matching->approve->name}}</span>
         @endif
       </div>
     </div>
   </div>
   <div class="c-user--Container">
-    <p class="c-user--featureTitle">評価<span class="c-form--required c-form--required--review">必須</span></p>
+    <p class="c-user--featureTitle--review">評価<span class="c-form--required c-form--required--review">必須</span></p>
     <div class="c-user--featureArea">
       <p class="c-user--body">
         <comment-star
@@ -33,14 +33,14 @@
     </div><br>
   </div>
   <div class="c-user--Container">
-    <p class="c-user--featureTitle">タイトル<span class="c-form--required c-form--required--review">必須</span></p><br>
-    <div class="c-user--featureArea">
+    <p class="c-user--featureTitle--review">タイトル<span class="c-form--required c-form--required--review">必須</span></p><br>
+    <div class="c-user--featureArea--review">
       <input type="text" name="title" value="{{ $review->title ?? old('title')}}" placeholder="タイトル名を入力してください" class="c-user--input">
     </div>
   </div>
   <div class="c-user--Container">
-    <p class="c-user--featureTitle">内容<span class="c-form--required c-form--required--review">必須</span></p>
-    <div class="c-user--featureArea">
+    <p class="c-user--featureTitle--review">内容<span class="c-form--required c-form--required--review">必須</span></p>
+    <div class="c-user--featureArea--review">
       <textarea name="body" value="" placeholder="内容を入力してください" class="c-user--text">{{ $review->body ?? old('body')}}</textarea>
     </div>
   </div>
