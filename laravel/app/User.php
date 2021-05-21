5cae8ebfa0776bc82ba0693ad15f5b13a6688f9f
masahiro96848 <feetwcpic9436@gmail.com>
2021-05-21 15:02:24 +0900

    ローカルでパスワードメール送信機能を追加　

--- laravel/app/User.php
+++ laravel/app/User.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use App\Notifications\PasswordResetNotification;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
